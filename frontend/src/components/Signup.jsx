0a4f7873f21120befc29e31a8a72bff8f19e2c3d
Suryodaya pandey <79349719+Suryodaya27@users.noreply.github.com>
2023-09-27 18:27:29 +0530

    added pagination

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -80,14 +80,20 @@ export function Signup() {
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
             <div className="mb-2 flex justify-center">
-              <svg
-                className="h-6 w-6 text-rose-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-              >
-                <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-              </svg>
+            <svg
+              className="h-8 w-8 text-black"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="9" cy="21" r="2"></circle>
+              <circle cx="20" cy="21" r="2"></circle>
+              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 1.9 1.61h10.84a2 2 0 0 0 1.9-1.61L23 6H6"></path>
+            </svg>
             </div>
             <h2 className="text-center text-2xl font-bold leading-tight text-black">
               Sign up to create an account
--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -4,6 +4,7 @@ import ProductCard from "../components/ProductCard";
 import { Navbar } from "../components/Navbar";
 import axios from "axios";
 import { Footer } from "../components/Footer";
+import { motion } from "framer-motion";
 
 async function fetchProducts() {
   try {
@@ -15,16 +16,36 @@ async function fetchProducts() {
 }
 
 function ProductList() {
-  const { data, error, isLoading } = useQuery("products", fetchProducts);
   const [categoryFilter, setCategoryFilter] = useState("all");
-  const [searchQuery, setSearchQuery] = useState(""); // State for search query
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { data, error, isLoading } = useQuery("products", fetchProducts);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="text-center text-xl mt-8"
+      >
+        Loading...
+      </motion.div>
+    );
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="text-center text-xl mt-8"
+      >
+        Error: {error.message}
+      </motion.div>
+    );
   }
 
   // Filter products based on category
@@ -38,58 +59,92 @@ function ProductList() {
     product.productName.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  // Calculate total number of pages based on the filtered data and page size
+  const pageSize = 10; // Number of products per page
+  const totalPages = Math.ceil(searchedData.length / pageSize);
+
+  // Calculate the starting index for the current page
+  const startIndex = (currentPage - 1) * pageSize;
+
+  // Slice the data to get the products for the current page
+  const productsForPage = searchedData.slice(startIndex, startIndex + pageSize);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
   return (
-    <>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
       <Navbar />
       <div className="mx-12 py-3">
-        <div className="flex justify-between mb-4">
-          {/* Category filtering UI */}
-          <div>
-            <label htmlFor="categoryFilter" className="mr-2">
-              Filter by Category:
-            </label>
-            <select
-              id="categoryFilter"
-              onChange={(e) => setCategoryFilter(e.target.value)}
-              value={categoryFilter}
-              className="px-2 py-1 border rounded-md"
-            >
-              <option value="all">All</option>
-              <option value="Fruits">Fruits</option>
-              <option value="Vegetables">Vegetables</option>
-              <option value="Dairy">Dairy</option>
-              <option value="Grains">Grains</option>
-              <option value="Bakery">Bakery</option>
-              <option value="Seafood">Seafood</option>
-              <option value="Nuts">Nuts</option>
-              <option value="Condiments">Condiments</option>
-            </select>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+          <div className="flex justify-between mb-4">
+            <div>
+              <label htmlFor="categoryFilter" className="mr-2">
+                Filter by Category:
+              </label>
+              <select
+                id="categoryFilter"
+                onChange={(e) => setCategoryFilter(e.target.value)}
+                value={categoryFilter}
+                className="px-2 py-1 border rounded-md"
+              >
+                <option value="all">All</option>
+                <option value="Fruits">Fruits</option>
+                <option value="Vegetables">Vegetables</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Grains">Grains</option>
+                <option value="Bakery">Bakery</option>
+                <option value="Seafood">Seafood</option>
+                <option value="Nuts">Nuts</option>
+                <option value="Condiments">Condiments</option>
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="search" className="mr-2">
+                Search:
+              </label>
+              <input
+                type="text"
+                id="search"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="px-2 py-1 border rounded-md"
+              />
+            </div>
           </div>
 
-          {/* Search input */}
-          <div>
-            <label htmlFor="search" className="mr-2">
-              Search:
-            </label>
-            <input
-              type="text"
-              id="search"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="px-2 py-1 border rounded-md"
-            />
+          <div className="flex flex-wrap justify-start gap-4">
+            {productsForPage.map((product) => (
+              <ProductCard key={product.productId} product={product} />
+            ))}
           </div>
-        </div>
 
-        {/* Display filtered products */}
-        <div className="flex flex-wrap justify-start gap-4">
-          {searchedData.map((product) => (
-            <ProductCard key={product.productId} product={product} />
-          ))}
-        </div>
+          <div className="flex justify-center mt-4">
+            <div className="flex items-center">
+              
+              {[...Array(totalPages)].map((_, page) => (
+                <a
+                  key={page + 1}
+                  href="#"
+                  className={`mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105 ${
+                    page + 1 === currentPage
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "hover:bg-gray-100"
+                  }`}
+                  onClick={() => handlePageChange(page + 1)}
+                >
+                  {page + 1}
+                </a>
+              ))}
+              
+            </div>
+          </div>
+        </motion.div>
       </div>
-      <Footer/>
-    </>
+      <Footer />
+    </motion.div>
   );
 }
 
